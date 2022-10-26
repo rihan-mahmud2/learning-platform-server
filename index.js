bcbac34227037bc0b02561c4859dbc6cb1b3f4ce
rihan-mahmud2 <rihanmahmud1415@gmail.com>
2022-10-26 12:33:58 +0600

    second commit

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 const port = 5000;
+const courses = require("./Data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("App is running");
+});
+
+app.get("/course", (req, res) => {
+  res.send(courses);
+  console.log(courses);
 });
 
 app.listen(5000, () => {
